a5dc2af2290d57613c51ce23dbdf07174b150e34
Paul D. Sheriff <35812299+PaulDSheriff@users.noreply.github.com>
2023-09-20 14:06:33 -0500

    XAML Basics - Resources for Labs

---
+++ XAML-Basics-Resources/EntityClasses/PhoneType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Common.Library;
+
+namespace AdventureWorks.EntityLayer;
+
+public class PhoneType : EntityBase
+{
+  #region Private Variables
+  private int _PhoneTypeId;
+  private string _TypeDescription = string.Empty;
+  #endregion
+
+  #region Public Properties
+  public int PhoneTypeId
+  {
+    get { return _PhoneTypeId; }
+    set {
+      _PhoneTypeId = value;
+      RaisePropertyChanged(nameof(PhoneTypeId));
+    }
+  }
+
+  public string TypeDescription
+  {
+    get { return _TypeDescription; }
+    set {
+      _TypeDescription = value;
+      RaisePropertyChanged(nameof(TypeDescription));
+    }
+  }
+  #endregion
+}
