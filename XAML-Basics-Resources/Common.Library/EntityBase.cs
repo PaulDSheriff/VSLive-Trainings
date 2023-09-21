c8f97c39a9e8fbaaf4f65fda7e467551bbbfc3ae
Paul D. Sheriff <35812299+PaulDSheriff@users.noreply.github.com>
2023-09-21 08:13:05 -0500

    XAML Basics - Resources for Labs

--- XAML-Basics-Resources/Common.Library/EntityBase.cs
+++ XAML-Basics-Resources/Common.Library/EntityBase.cs
@@ -1,17 +1,5 @@
 ﻿namespace Common.Library;
 
-public class EntityBase : CommonBase {
-  #region Constructor
-  public EntityBase() {
-    Init();
-  }
-  #endregion
-
-  #region Init Method
-  /// <summary>
-  /// Initialize any properties of this class
-  /// </summary>
-  public override void Init() {
-  }
-  #endregion
+public class EntityBase : CommonBase
+{
 }
