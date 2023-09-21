d119527399a219b6bc9f2eff4a56d41201be23c2
Paul D. Sheriff <35812299+PaulDSheriff@users.noreply.github.com>
2023-09-21 14:12:49 -0500

    Add files via upload

---
+++ MAUI-Basics-Labs/09-Application/Labs-Answers/AdventureWorks.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventureWorks.MAUI
+{
+  public partial class AppShell : Shell
+  {
+    public AppShell()
+    {
+      InitializeComponent();
+    }
+  }
+}
---
+++ MAUI-Basics-Labs/09-Application/Labs-Answers/AdventureWorks.MAUI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventureWorks.MAUI
+{
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+  }
+}
