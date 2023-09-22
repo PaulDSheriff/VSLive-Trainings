f3e2943365f55815836ddc73d86237612b68a709
Paul D. Sheriff <35812299+PaulDSheriff@users.noreply.github.com>
2023-09-22 12:57:19 -0500

    Add files via upload

---
+++ WPF-Basics-Labs/04-Basics/Labs-Answers/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace XamlBasicsLabWPF {
+  public partial class MainWindow : Window {
+    public MainWindow() {
+      InitializeComponent();
+    }
+
+    private void SaveButton_Click(object sender, RoutedEventArgs e) {
+      MessageBox.Show("User Saved");
+    }
+  }
+}
