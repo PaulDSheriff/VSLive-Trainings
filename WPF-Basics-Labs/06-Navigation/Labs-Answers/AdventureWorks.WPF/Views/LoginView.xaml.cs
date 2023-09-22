7022f91fc931607110bfa559e93ebefec9aa6be5
Paul D. Sheriff <35812299+PaulDSheriff@users.noreply.github.com>
2023-09-22 12:58:01 -0500

    Add files via upload

---
+++ WPF-Basics-Labs/06-Navigation/Labs-Answers/AdventureWorks.WPF/Views/LoginView.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace AdventureWorks.WPF.Views {
+  public partial class LoginView : UserControl {
+    public LoginView() {
+      InitializeComponent();
+    }
+  }
+}
