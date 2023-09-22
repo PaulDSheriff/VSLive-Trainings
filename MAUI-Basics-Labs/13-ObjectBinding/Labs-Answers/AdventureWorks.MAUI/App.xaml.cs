9e49820a03c271a307b3f0fb01d7b866a06e50ae
Paul D. Sheriff <35812299+PaulDSheriff@users.noreply.github.com>
2023-09-22 09:38:59 -0500

    Add files via upload

---
+++ MAUI-Basics-Labs/13-ObjectBinding/Labs-Answers/AdventureWorks.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AdventureWorks.MAUI
+{
+  public partial class App : Application
+  {
+    public App()
+    {
+      InitializeComponent();
+
+      MainPage = new AppShell();
+    }
+  }
+}
